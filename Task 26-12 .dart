e7f260e62104bf4c4d5de384eab12271cdb4bb31
miteshdabhi30 <154596969+miteshdabhi30@users.noreply.github.com>
2023-12-29 20:05:58 +0530

    Tasks

---
+++ Task 26-12 .dart
@@ -0,0 +1,62 @@
+void main()
+{
+  print("My name is Mitesh Dabhi");
+  print ("My birthdate is 30th sep 1998 ");
+  print ("I am 25 year old");
+  print ("I am from rajkot");
+
+  int a=10;
+  int b=20;
+  int sum = a+b;
+  print(sum);
+
+  int sub = a-b;
+  print(sub);
+
+  int mul = a*b;
+  print(mul);
+
+  double div = a/b;
+  print(div);
+
+  int L=5;
+  int H=8;
+  int area = L*H;
+  print("area of square : $area");
+
+
+  int L=15;
+  int H=10;
+  int W=12;
+  int area = L*H*W;
+  print("area of cube = $area");
+
+  double PI = 3.14;
+  int R = 4;
+  double area = PI*R*R;
+  print("are of circle = $area");
+
+  int H=20;
+  int B = 10;
+  double area = H*B/2;
+  print("area of triangle = $area");
+
+  int Principle = 500000;
+  int Rate = 5;
+  int Time = 1;
+  double SI = Principle*Rate*Time/100;
+  print ("simple intrest = $SI");
+
+  int G = 80;
+  int H = 75;
+  int E = 80;
+  int M = 90;
+  int S = 85;
+  double percentage = (G+H+E+M+S)/5;
+  print(percentage);
+
+
+
+
+
+}
