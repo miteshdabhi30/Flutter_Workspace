8671f224b58fcffc66cb7adcb8c74b2bb061ae74
miteshdabhi30 <154596969+miteshdabhi30@users.noreply.github.com>
2024-03-02 16:50:47 +0530

    Delete Task(30-12) directory

--- Task(30-12)/For in Loop.dart
+++
@@ -1,9 +0,0 @@
-void main()
-{
-  List numberlist = [1,2,3,4,5,6,7,8,9,10];
-
-  for(int List in numberlist)
-    {
-      print(List);
-    }
-}
--- Task(30-12)/While Loop.dart
+++
@@ -1,9 +0,0 @@
-void main()
-{
-  int a = 51;
-  while(a<=60)
-    {
-      print(a);
-      a++;
-    }
-}
