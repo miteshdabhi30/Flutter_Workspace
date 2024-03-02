897405a890eeacf56dd1267ea1f1a939fa643912
miteshdabhi30 <154596969+miteshdabhi30@users.noreply.github.com>
2024-03-02 17:46:53 +0530

    Delete Print Basic Details.dart

--- Print Basic Details.dart
+++
@@ -1,8 +0,0 @@
-void main()
-{
-  print("My Name is Mitesh");
-  print("My Surname is Dabhi");
-  print("My Birthdate is 30 sep 1998");
-  print("I am 26 Year old");
-  print("I am from Rajkot Gujarat");
-}
