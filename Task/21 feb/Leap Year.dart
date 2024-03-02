c994ddde34b2bf629aafd1f21ce8cd8ff3d65e47
miteshdabhi30 <154596969+miteshdabhi30@users.noreply.github.com>
2024-03-02 17:49:47 +0530

    Add files via upload

---
+++ Task/21 feb/Leap Year.dart
@@ -0,0 +1,17 @@
+void main()
+{
+  int day = 366;
+
+  if(day==365)
+    {
+      print("This is Not a Leap Year");
+    }
+  else if(day==366)
+  {
+    print("This is Leap Year");
+  }
+  else
+    {
+      print("Error");
+    }
+}
